25705aa8743a7c05abdf42bc203bf48a9f5b2908
berrrchik <berrrchik@mail.ru>
2026-03-26 22:12:02 +0300

    feat: add video repository error type

---
+++ app/src/main/java/com/rsl/dictionary/errors/VideoRepositoryError.kt
@@ -0,0 +1,8 @@
+package com.rsl.dictionary.errors
+
+sealed class VideoRepositoryError(cause: Throwable? = null) : Exception(cause) {
+    object UrlNotFound : VideoRepositoryError()
+    data class DownloadFailed(override val cause: Throwable) : VideoRepositoryError(cause)
+    data class CacheError(override val cause: Throwable) : VideoRepositoryError(cause)
+    object UnknownError : VideoRepositoryError()
+}
