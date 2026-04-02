6e42618dcfc7fe88373908671568ee832c3e44d0
berrrchik <berrrchik@mail.ru>
2026-04-02 15:31:37 +0300

    feat: add Compose main activity

--- app/src/main/java/com/rsl/dictionary/MainActivity.kt
+++ app/src/main/java/com/rsl/dictionary/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.rsl.dictionary
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.rsl.dictionary.ui.screens.MainScreen
+import com.rsl.dictionary.ui.theme.RussianSignLanguageDictionaryAndroidTheme as RslDictionaryTheme
+import com.rsl.dictionary.viewmodels.StartupViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private val startupViewModel: StartupViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContent {
+            RslDictionaryTheme {
+                MainScreen(startupViewModel = startupViewModel)
+            }
         }
     }
 }
