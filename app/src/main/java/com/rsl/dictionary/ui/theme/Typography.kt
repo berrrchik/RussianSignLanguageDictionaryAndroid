acebc0cb74471b0d52519f44c7582188a38cb5e2
berrrchik <berrrchik@mail.ru>
2026-04-02 15:28:44 +0300

    feat: add app typography

---
+++ app/src/main/java/com/rsl/dictionary/ui/theme/Typography.kt
@@ -0,0 +1,34 @@
+package com.rsl.dictionary.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    headlineSmall = TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 32.sp
+    ),
+    titleMedium = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 24.sp
+    ),
+    bodyLarge = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontSize = 11.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp
+    )
+)
