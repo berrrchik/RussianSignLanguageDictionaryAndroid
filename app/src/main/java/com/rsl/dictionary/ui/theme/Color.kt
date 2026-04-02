9a20571ef082b1d0300a0adb5e131a9840eb0cae
berrrchik <berrrchik@mail.ru>
2026-04-02 15:28:30 +0300

    feat: add app color palette

---
+++ app/src/main/java/com/rsl/dictionary/ui/theme/Color.kt
@@ -0,0 +1,35 @@
+package com.rsl.dictionary.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF007AFF)
+val OnPrimaryBlue = Color(0xFFFFFFFF)
+val PrimaryBlueContainer = Color(0xFFDCEBFF)
+val OnPrimaryBlueContainer = Color(0xFF001C3A)
+val InversePrimaryBlue = Color(0xFFA8CCFF)
+
+val SecondaryGray = Color(0xFF6C757D)
+val OnSecondaryGray = Color(0xFFFFFFFF)
+val SecondaryGrayContainer = Color(0xFFE9ECEF)
+val OnSecondaryGrayContainer = Color(0xFF1F2328)
+
+val BackgroundLight = Color(0xFFF8F9FA)
+val OnBackgroundLight = Color(0xFF111418)
+val SurfaceLight = Color(0xFFFFFFFF)
+val OnSurfaceLight = Color(0xFF111418)
+val SurfaceVariantLight = Color(0xFFF1F4F7)
+val OnSurfaceVariantLight = Color(0xFF5F6B76)
+val SurfaceDimLight = Color(0xFFF1F3F5)
+val SurfaceBrightLight = Color(0xFFFFFFFF)
+val SurfaceContainerLowLight = Color(0xFFFCFCFD)
+val SurfaceContainerLight = Color(0xFFF7F9FB)
+val SurfaceContainerHighLight = Color(0xFFF1F4F7)
+val SurfaceContainerHighestLight = Color(0xFFE9EEF3)
+val InverseSurfaceDark = Color(0xFF1F2328)
+val InverseOnSurfaceLight = Color(0xFFF8F9FA)
+val OutlineLight = Color(0xFFD0D7DE)
+val OutlineVariantLight = Color(0xFFE1E7ED)
+val ScrimDark = Color(0x66000000)
+
+val ErrorRed = Color(0xFFD92D20)
+val OnErrorRed = Color(0xFFFFFFFF)
